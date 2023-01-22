4c53de220b4c8820f8757d0052007879428fc7ea
Rakeshuser <rakesh611999@gmail.com>
2023-01-22 23:08:39 +0530

    new message

---
+++ src/org/client/Client.java
@@ -0,0 +1,16 @@
+package org.client;
+
+public class Client {
+
+	public void clientName() {
+	   System.out.println("CTS");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Client c = new Client();
+		c.clientName();
+		
+	}
+}
---
+++ src/org/company/Company.java
@@ -0,0 +1,19 @@
+package org.company;
+
+import org.client.Client;
+
+public class Company extends Client {
+
+	private void companyName() {
+		System.out.println("HCL");
+
+	}
+	
+	public static void main (String[]args) {
+		
+		Company c = new Company();
+		c.companyName();
+		
+		c.clientName();
+	}
+}
